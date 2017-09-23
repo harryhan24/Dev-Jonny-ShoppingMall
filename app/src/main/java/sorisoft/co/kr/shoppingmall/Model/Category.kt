5433684db7055f01ba1626624f0388c56206f282
harryhan24 <herryhan2435@gmail.com>
2017-09-24 06:03:03 +0900

    lesson 01

---
+++ app/src/main/java/sorisoft/co/kr/shoppingmall/Model/Category.kt
@@ -0,0 +1,5 @@
+package sorisoft.co.kr.shoppingmall.Model
+
+/**
+ * Created by harry on 2017. 9. 24..
+ */
---
+++ app/src/main/java/sorisoft/co/kr/shoppingmall/Services/DataService.kt
@@ -0,0 +1,7 @@
+package sorisoft.co.kr.shoppingmall.Services
+
+/**
+ * Created by harry on 2017. 9. 24..
+ */
+object DataService {
+}
